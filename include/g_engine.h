5484361ee1029ffc4c149bbd0ecbd8d637330523
klh <>
2001-10-25 13:20:02 +0000

    Changed method for main d.p. engine. See g_engine.c for more details

--- include/g_engine.h
+++ include/g_engine.h
@@ -1,4 +1,4 @@
-/*  Last edited: Oct 15 13:51 2001 (klh) */
+/*  Last edited: Oct 25 14:09 2001 (klh) */
 /**********************************************************************
  ** File: engine.h
  ** Author : Kevin Howe
@@ -17,10 +17,14 @@ typedef struct {
   double score;
   int last_selected;
   GArray *fringe_idx;
-} Gaze_DP_struct;
 
-void free_Gaze_DP_struct( Gaze_DP_struct * );
-Gaze_DP_struct *new_Gaze_DP_struct( int );
+  GArray ***feats;   /* indices of features processed so far, organised by type */
+  int ***fringes;    /* indices of last "significant" feature, organised first by
+		        target type, then by source type, then by frame */
+} Gaze_DP_struct;    
+
+void free_Gaze_DP_struct( Gaze_DP_struct *, int );
+Gaze_DP_struct *new_Gaze_DP_struct( int, int );
 
 
 void calculate_path_score(GArray *, GArray *, Gaze_Structure *);
@@ -38,6 +42,8 @@ void backwards_calc(GArray *,
 		    enum DP_Calc_Mode,
 		    int,
 		    FILE *);
+
+
 
 void scan_through_sources_dp(GArray *,
 			     GArray *,
@@ -58,11 +64,31 @@ void scan_through_targets_dp(GArray *,
 			     int,
 			     FILE *);
 
+void scan_through_sources_dp_old(GArray *,
+				 GArray *,
+				 int,
+				 Gaze_DP_struct *,
+				 Gaze_Structure *,
+				 enum DP_Calc_Mode,
+				 enum DP_Traceback_Mode,
+				 int,
+				 FILE *);
+
+void scan_through_targets_dp_old(GArray *,
+				 GArray *,
+				 int,
+				 Gaze_DP_struct *,
+				 Gaze_Structure *,
+				 enum DP_Calc_Mode,
+				 int,
+				 FILE *);
+
+
+
 GArray *trace_back_general(GArray *,
 			   GArray *,
 			   Gaze_Structure *,
 			   enum DP_Traceback_Mode);
-
 
 #endif
 
